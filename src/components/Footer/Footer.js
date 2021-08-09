ecb1e80b2b2149377e249a88ed4a90c8867aa3e9
GaryMcCulloch <garymcculloch1@gmail.com>
2021-08-09 15:20:08 +0100

    Navbar and Footer created

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,175 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Footer() {
+    return (
+        <FooterStyled>
+            <div className="container">
+                <div className="row">
+                    <div className="footer-col">
+                        <h2>Story</h2>
+                        <ul>
+                            <li><a href="#">Our Story</a></li>
+                            <li><a href="#">Clients</a></li>
+                            <li><a href="#">Testimonials</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h2>Services</h2>
+                        <ul>
+                            <li><a href="#">Marketing</a></li>
+                            <li><a href="#">Consulting</a></li>
+                            <li><a href="#">Development</a></li>
+                            <li><a href="#">Design</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h2>Contact</h2>
+                        <ul>
+                            <li><a href="#">Email</a></li>
+                            <li><a href="#">Phone</a></li>
+                            <li><a href="#">In Person</a></li>
+                            <li><a href="#">Support</a></li>
+                        </ul>
+                    </div>
+                    <div className="footer-col social">
+                        <h2>Social</h2>
+                        <ul>
+                            <li><i className='fab fa-facebook-f'></i></li>
+                            <li><i className='fab fa-twitter'></i></li>
+                            <li><i className='fab fa-instagram'></i></li>
+                            <li><i className='fab fa-linkedin'></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </FooterStyled>
+    )
+}
+
+const FooterStyled = styled.div`
+    background: linear-gradient(90deg, rgb(110 ,94, 254) 0%, rgba(73, 63,252,1) 100%);
+    padding: 70px 0;
+    margin-top: 100vh;
+    .container {
+        max-width: 1170px;
+        margin: auto;
+    }
+    .row {
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .footer-col {
+        width: 25%;
+        padding: 0 15px;
+        text-align: center;
+    }
+    .footer-col h2 {
+        font-size: 18px;
+        color: #fff;
+        text-transform: capitalize;
+        margin-bottom: 30px;
+        font-weight: 500;
+        position: relative;
+    }
+    .footer-col h2::before {
+        content: '';
+        position: absolute;
+        left: 50%;
+        bottom: -7px;
+        background: aqua;
+        height: 2px;
+        box-sizing: border-box;
+        width: 50px;
+    }
+    .footer-col ul li:not(:last-child) {
+        margin-bottom: 10px;
+    }
+    .footer-col ul li a{
+        font-size: 16px;
+        text-transform: capitalize;
+        color: #fff;
+        text-decoration: none;
+        font-weight: 300;
+        color: #bbbbbb;
+        display: block;
+        transition: all 0.3s ease;
+    }
+    .footer-col ul li a:hover {
+        color: #fff;
+        padding-left: 4px;
+    }
+    .social ul{
+        display: flex;    
+        justify-content: center;
+    }
+    
+    .social ul li {
+        height: 40px;
+        width: 40px;
+        background: rgba(255, 255, 255, 0.2);
+        margin: 0 10px 10px 0;;
+        line-height: 40px;
+        border-radius: 50%;
+        color: #fff;
+        transition: all 0.5s ease;
+    }
+    .social ul li:hover {
+        color: #24262b;
+        background: #fff;
+    }
+    @media(max-width: 767px) {
+        .footer-col {
+            width: 50%;
+            margin-bottom: 30px;
+        }
+    }
+
+    @media(max-width: 574px) {
+        .footer-col {
+            width: 100%;
+            margin-bottom: 30px;
+        }
+    }
+`;
+
+export default Footer;
+
+
+
+// .container{
+//     h1{
+//         display: none;
+//     }
+//     width: 100%;
+//     height: auto;
+//     background: linear-gradient(90deg, rgb(110 ,94, 254) 0%, rgba(73, 63,252,1) 100%);
+//     display: grid;
+//     justify-content: center;
+//     align-content: center;
+//     display: grid;
+//     gap: 10rem;
+//     grid-auto-flow: column;
+    
+//     .footer-item{
+//         color: #fff;
+//         justify-self: center;
+
+//     }
+//     ul{
+//         padding: 0;
+//     }
+//     li{
+//         list-style-type: none;
+//         padding: .7rem 0;
+//         cursor: pointer;
+//     }
+
+//     @media only screen and (max-width: 1000px){
+//         .container{
+//             h1{
+//         display: block;
+//     }
+//         }
+//     }
+// }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,189 @@
+// Font Awesome used for menu icons 
+//<script src="https://kit.fontawesome.com/b616ef5313.js" crossorigin="anonymous"></script> *** to be placed in html head
+
+// styled components used - must install to work *** npm install --save styled-components
+
+// map used to display nav links
+
+
+import React, { useState } from 'react';
+import { MenuItems } from './MenuItems';
+import styled from 'styled-components';
+
+
+function Navbar() {
+    const [navToggle, setNavToggle] = useState(false);
+
+    return (
+        
+        <NavbarStyled>
+            <div className="navbar-items">
+                <h1 className="navbar-logo">Navbar</h1>
+                <div className="menu-icon" onClick={() => setNavToggle(!navToggle)}>
+                    <i className={navToggle ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={navToggle ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItems.map((item, index) => {
+                        return (
+                            <li>
+                                <a className={item.cName} href={item.url}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <button>Shop Now</button>
+            </div>
+        </NavbarStyled>
+    )
+}
+
+const NavbarStyled = styled.nav`
+    .navbar-items{
+    background: linear-gradient(90deg, rgb(110 ,94, 254) 0%, rgba(73, 63,252,1) 100%);
+    height: 80px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.2rem;
+  }
+
+  .navbar-logo{
+    color: white;
+    justify-self: start;
+    margin-left: 20px;
+    cursor: pointer;
+  }
+  .nav-menu{
+      display: grid;
+      grid-template-columns: repeat(5, auto);
+      grid-gap: 10px;
+      list-style: none;
+      text-align: center;
+      width: 70vw;
+      justify-content: end;
+      margin-right: 2rem;
+  }
+  .nav-links{
+      color: white;
+      text-decoration: none;
+      padding: 0.5rem 1rem;
+  }
+  .nav-links:hover{
+      background-color: #6d76f7;
+      border-radius: 4px;
+      transition: all 0.2 ease-out;
+  }
+
+  .fa-bars {
+      color: #fff;
+  }
+  .nav-links-mobile{
+    display: none;
+  }
+  .menu-icon{
+      display: none;
+  }
+
+  button{
+      padding: 8px 20px;
+      border-radius: 4px;
+      outline: none;
+      border: none;
+      cursor: pointer;
+      background: #3acbf7;
+      color: #fff;
+      margin: 0 20px;
+  }
+  button:hover{
+      padding: 8px 20px;
+      transition: all 0.3s ease-out;
+      background: #fff;
+      color: #6568F4;
+      transition: 250ms;
+  }
+
+  @media screen and (max-width: 790px) {
+      .navbar-items {
+          position: relative;
+      }
+      .nav-menu {
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          width: 100%;
+          height: 500px;
+          position: absolute;
+          top: 80px;
+          left: -100%;
+          opacity: 1;
+          transition: all 0.5s ease;
+          margin: 0;
+          padding: 0;
+          justify-content: space-evenly;
+      }
+      .nav-menu.active {
+          background: #6668f4;
+          left: 0;
+          opacity: 1;
+          transition: all 0.5s ease;
+          z-index: 1;
+          justify-content: space-evenly;
+          
+      }
+      
+      .nav-links {
+          text-align: center;
+          width: 100%;
+          display: table;
+          
+      }
+      .nav-links:hover {
+          background: #7577fa;
+          border-radius: 4px;
+          
+      }
+      .navbar-logo {
+          position: absolute;
+          top: 0;
+          left: 0;
+          transform: translate(25%, 60%);
+      }
+      .menu-icon {
+          display: block;
+          position: absolute;
+          top: 0;
+          right: 0;
+          transform: translate(-100%, 60%);
+          font-size: 1.8rem;
+          cursor: pointer;
+      }
+      .fa-times {
+          color: #fff;
+          font-size: 2rem;
+      }
+      .nav-links-mobile {
+          display: block;
+          text-align: center;
+          padding: 1.5rem;
+          margin: 2rem auto;
+          border-radius: 4px;
+          width: 80%;
+          background: #3acbf7;
+          text-decoration: none;
+          color: #fff;
+          font-size: 1.5rem;
+      }
+      .nav-links-mobile:hover {
+          background: #fff;
+          color: #6568F4;
+          transition: 250ms;
+      }
+      button {
+          display: none;
+      }
+  }
+`;
+
+export default Navbar
